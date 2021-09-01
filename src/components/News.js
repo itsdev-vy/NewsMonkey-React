69f522d091b4273020315d2be9fbad08d9467669
itsdev-vy <itsvinayadav@gmail.com>
2021-09-01 16:29:44 +0530

    Hiding API Key by Adding Custom Environment Variables

--- src/components/News.js
+++ src/components/News.js
@@ -37,7 +37,7 @@ export class News extends Component {
 
         async updateNews(){
             this.props.setProgress(10);
-            const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a9d3afa1e1a444ff92ddce370befbace&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+            const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
             this.setState({loading: true});
             let data = await fetch(url);
             this.props.setProgress(30);
@@ -66,7 +66,7 @@ export class News extends Component {
 
          fetchMoreData = async () => {  
         this.setState({page: this.state.page + 1})
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a9d3afa1e1a444ff92ddce370befbace&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        const url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
