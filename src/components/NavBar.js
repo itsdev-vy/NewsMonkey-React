1b7e62e91e5b5cd33698dbfea23931772a9fb667
itsdev-vy <itsvinayadav@gmail.com>
2021-09-01 17:58:42 +0530

    Sticky Navbar & NewsMonkey bug fixes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react';
 import {Link} from "react-router-dom";
 
-export class NavBar extends Component {
-
-        render() {
+const NavBar = () =>{
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
                         <Link className="navbar-brand" to="/">NewsMonkey</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,15 +21,11 @@ export class NavBar extends Component {
                             <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
                             <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
-
-
                         </ul>
                         </div>
                     </div>
                     </nav>
             </div>
         )
-    }
 }
-
 export default NavBar
